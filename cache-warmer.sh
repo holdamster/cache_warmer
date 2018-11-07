60822e433179fcc7a14940cd86a6809e5b55459e
holdamster <41182979+holdamster@users.noreply.github.com>
2018-11-07 12:29:15 +0100

    Update cache-warmer.sh

--- cache-warmer.sh
+++ cache-warmer.sh
@@ -23,7 +23,6 @@ cat "$1" | while read line; do
 	echo -e "$line\n" >> /root/cache-warmer/tmp/$NUMBER
 	curl -A "$USERAGENT" -so /dev/null -w "@/root/cache-warmer/curl-format.txt" $line >> /root/cache-warmer/tmp/$NUMBER
 
-	ADDRESS=`grep -i <YOUR_DOMAIN_NAME> /root/cache-warmer/tmp/$NUMBER`
 	TIME_NAMELOOKUP=`grep -i time_namelookup /root/cache-warmer/tmp/$NUMBER | awk '{print $2}'`
 	TIME_CONNECT=`grep -i time_connect /root/cache-warmer/tmp/$NUMBER	| awk '{print $2}'`
 	TIME_APPCONNECT=`grep -i time_appconnect /root/cache-warmer/tmp/$NUMBER	| awk '{print $2}'`
@@ -32,6 +31,6 @@ cat "$1" | while read line; do
 	TIME_STARTTRANSFER=`grep -i time_starttransfer /root/cache-warmer/tmp/$NUMBER	| awk '{print $2}'`
 	TIME_TOTAL=`grep -i time_total /root/cache-warmer/tmp/$NUMBER	| awk '{print $2}'`
 
-	mysql -D stats -e "INSERT INTO table_name (address, id, timestamp, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, time_total) VALUES ('$ADDRESS', NULL, current_timestamp(), '$TIME_NAMELOOKUP', '$TIME_CONNECT', '$TIME_APPCONNECT', '$TIME_PRETRANSFER', '$TIME_REDIRECT', '$TIME_STARTTRANSFER', '$TIME_TOTAL');"
+	mysql -D stats -e "INSERT INTO table_name (address, id, timestamp, time_namelookup, time_connect, time_appconnect, time_pretransfer, time_redirect, time_starttransfer, time_total) VALUES ('$line', NULL, current_timestamp(), '$TIME_NAMELOOKUP', '$TIME_CONNECT', '$TIME_APPCONNECT', '$TIME_PRETRANSFER', '$TIME_REDIRECT', '$TIME_STARTTRANSFER', '$TIME_TOTAL');"
 
 done
